81d4f46f6df04bd9f01b023f81930d9e9ed2798e
duha6861 <duha6861@colorado.edu>
2024-02-07 23:41:51 -0700

    Problem 4 part b

--- Homework/Homework 2/Problem4.py
+++ Homework/Homework 2/Problem4.py
@@ -12,3 +12,41 @@ for i in range(N):
     S = t[i]*y[i]
 
 print('The sum is:', S)
+
+
+# Part b
+
+theta  = np.linspace(0, 2*np.pi, 1000)
+R = 1.2
+dr = 0.1
+f = 15
+p=0
+
+x = R*(1 + dr*np.sin(f*theta + p))*np.cos(theta)
+y = R*(1 + dr*np.sin(f*theta + p))*np.sin(theta)
+
+plt.figure(1)
+plt.plot(x,y)
+plt.xlim(-1.5, 1.5)
+plt.ylim(-1.5, 1.5)
+plt.show()
+
+
+plt.figure(2)
+
+i = 1
+N = 10
+
+for i in range(N):
+    R = i
+    f = 2 + i
+    dr = 0.05
+    p = np.random.uniform(0, 2)
+
+    x2 = R*(1 + dr*np.sin(f*theta + p))*np.cos(theta)
+    y2 = R*(1 + dr*np.sin(f*theta + p))*np.sin(theta)
+
+    plt.plot(x2, y2)
+plt.show()
+
+ 
