749365aa1047c4449c0dc97c8c06fe602bbe327b
duha6861 <duha6861@colorado.edu>
2024-02-16 17:53:18 -0700

    HW3 Problem 3

---
+++ Homework/Homework 3/Problem3.py
@@ -0,0 +1,83 @@
+# import libraries
+import numpy as np
+
+
+# define routines
+def bisection(f,a,b,tol,Nmax):
+    '''
+    Inputs:
+      f,a,b       - function and endpoints of initial interval
+      tol, Nmax   - bisection stops when interval length < tol
+                  - or if Nmax iterations have occured
+    Returns:
+      astar - approximation of root
+      ier   - error message
+            - ier = 1 => cannot tell if there is a root in the interval
+            - ier = 0 == success
+            - ier = 2 => ran out of iterations
+            - ier = 3 => other error ==== You can explain
+    '''
+
+    '''     first verify there is a root we can find in the interval '''
+    fa = f(a); fb = f(b);
+
+    count = 0
+
+    if (fa*fb>0):
+       ier = 1
+       astar = a
+       return [astar, ier, count]
+
+    ''' verify end point is not a root '''
+    if (fa == 0):
+      astar = a
+      ier =0
+      return [astar, ier, count]
+
+    if (fb ==0):
+      astar = b
+      ier = 0
+      return [astar, ier, count]
+
+    while (count < Nmax):
+      c = 0.5*(a+b)
+      fc = f(c)
+
+      if (fc ==0):
+        astar = c
+        ier = 0
+        return [astar, ier, count]
+
+      if (fa*fc<0):
+         b = c
+      elif (fb*fc<0):
+        a = c
+        fa = fc
+      else:
+        astar = c
+        ier = 3
+        return [astar, ier, count]
+
+      if (abs(b-a)<tol):
+        astar = a
+        ier =0
+        return [astar, ier, count]
+      
+      count = count + 1
+
+    astar = a
+    ier = 2
+    return [astar,ier, count] 
+
+# use routines    
+f1 = lambda x: x**3 + x - 4
+a = 1
+b = 4
+
+Nmax = 1000
+tol = 1e-3
+
+[astar1,ier1,count1] = bisection(f1,a,b,tol,Nmax)
+print('The approximate root is',astar1)
+print('The error message reads:',ier1)
+print('The number of iterations is', count1)
