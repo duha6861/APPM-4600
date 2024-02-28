c00454eb3573c36faf3777f953b28142e2e1d809
duha6861 <duha6861@colorado.edu>
2024-02-27 22:37:57 -0700

    Code for Newton with approximate Jacobian

--- Labs/Lab6/Lab6.py
+++ Labs/Lab6/Lab6.py
@@ -17,9 +17,8 @@ def evalF(x):
     
 def evalJ(x): 
 
-    
-    J = np.array([8*x[0], 2*x[1]],
-                 [(1 - np.cos(x[0] - x[1])), (1 + np.cos(x[0] - x[1]))])
+    J = np.array([[8*x[0], 2*x[1]],
+                 [(1 - np.cos(x[0] - x[1])), (1 + np.cos(x[0] - x[1]))]])
     return J
 
 
@@ -75,10 +74,47 @@ def LazyNewton(x0,tol,Nmax):
     ier = 1
     return[xstar,ier,its]   
 
+def ForDiff(f, s, h):
+    f_prime = (f(s + h) - f(s))/h
+    return (f_prime)
+
+def CenDiff(f, s, h):
+    f_prime = (f(s + h) - f(s - h))/(2*h)
+    return (f_prime)
+
+def NewtonApprox(x0,tol,Nmax):
+  
+    ''' inputs: x0 = initial guess, tol = tolerance, Nmax = max its'''
+    ''' Outputs: xstar= approx root, ier = error message, its = num its'''
+
+    h = 1e-7
+
+    for its in range(Nmax):
+       F = evalF(x0)
+       F1 = evalF(x0 + np.array([h, 0]))
+       F2 = evalF(x0 + np.array([0, h]))
+
+       J = np.array([[(F1[0]-F[0])/h, (F2[0]-F[0])/h],
+                     [(F1[1]-F[1])/h, (F2[1]-F[1])/h]])
+       Jinv = inv(J)
+
+       x1 = x0 - Jinv.dot(F)
+       
+       if (norm(x1-x0) < tol):
+           xstar = x1
+           ier =0
+           return[xstar, ier, its]
+           
+       x0 = x1
+    
+    xstar = x1
+    ier = 1
+    return[xstar,ier,its]
+
 # use routines
 x0 = np.array([0, 1])
 
-Nmax = 100
+Nmax = 1000
 tol = 1e-10
 
 #t = time.time()
@@ -98,7 +134,16 @@ print(xstar)
 print('Lazy Newton: the error message reads:',ier)
 print('Lazy Newton: took this many seconds:',elapsed/20)
 print('Lazy Newton: number of iterations is:',its)
- 
+
+t = time.time()
+for j in range(20):
+  [xstar,ier,its] =  NewtonApprox(x0,tol,Nmax)
+elapsed = time.time()-t
+print(xstar)
+print('Approximate Newton: the error message reads:',ier)
+print('Approximate Newton: took this many seconds:',elapsed/20)
+print('Approximate Newton: number of iterations is:',its)
+
 '''t = time.time()
 for j in range(20):
   [xstar,ier,its] = Broyden(x0, tol,Nmax)     
