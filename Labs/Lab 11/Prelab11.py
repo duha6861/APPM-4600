059f06e8153dd43c224c8ba4983e638ce3a2b387
duha6861 <duha6861@colorado.edu>
2024-04-09 09:43:31 -0600

    Lab 11 code

--- Labs/Lab 11/Prelab11.py
+++ Labs/Lab 11/Prelab11.py
@@ -9,9 +9,8 @@ def trapezoidal(a,b,N,f):
 
     return(area)
 
-def comp_trapezoidal(a,b,N,f,f2):
+def comp_trapezoidal(a,b,N,f):
     h = (b - a)/N
-    u = (a + b)/2
     area = f(a)*h/2
     x = a + h
 
@@ -19,6 +18,7 @@ def comp_trapezoidal(a,b,N,f,f2):
         area = area + h * f(x)
         x = x + h
     
-    area = area + f(b)*h/2 #- (b - a)/12*h**2*f2(u)
+    area = area + f(b)*h/2
 
     return(area)
+
