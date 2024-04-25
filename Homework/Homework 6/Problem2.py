ccba250ec4eb98056926d70209b9ce307131168b
duha6861 <duha6861@colorado.edu>
2024-04-24 20:06:54 -0600

    HW6 P2 code

--- Homework/Homework 6/Problem2.py
+++ Homework/Homework 6/Problem2.py
@@ -5,6 +5,8 @@ def driver():
     a = -5
     b = 5
     f = lambda s: 1/(1 + s**2)
+    f2 = lambda s: (-2*(-3*s**2 + 1))/((1+s**2)**3)
+    f4 = lambda s: (24*(5*s**4 - 10*s**2 + 1)/((1+s**2)**5))
     N_trap = 1000
     N_simp = 1000
 
@@ -13,10 +15,18 @@ def driver():
     trap_area = comp_Trap(a,b,N_trap,f)
     print('Area for composite trapezoidal is: ', trap_area)
     print('Error is: ', np.abs(2*math.atan(5) - trap_area))
+    print('1: ', f2(-1),
+          '2: ', f2(0), 
+          '3: ', f2(1))
 
     simp_area = comp_Simp(a,b,N_simp,f)
     print("Area for composite Simpson's is:", simp_area)
     print('Error is: ', np.abs(2*math.atan(5) - simp_area))
+    print('1: ', f4(-np.sqrt(3)), 
+          '2: ', f4(-np.sqrt(1/3)), 
+          '3: ', f4(0), 
+          '4: ', f4(np.sqrt(1/3)),
+          '5: ', f4(np.sqrt(3)))
 
 def comp_Trap(a,b,N,f):
     h = (b-a)/N
